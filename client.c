7db4277d5c2f32ac87084406f3bcbb1000f156dd
Corentin Musard <corentinmusard@gmail.com>
2019-10-09 22:53:55 +0200

    client: add a clean exit when client receive SIGINT

--- client.c
+++ client.c
@@ -8,11 +8,17 @@
 #include <curses.h>
 #include <string.h>
 #include <time.h>
+#include <signal.h>
 
 #include "cli.h"
 #include "asynchronous.h"
 #include "utils.h"
 
+volatile sig_atomic_t sigintRaised = 0;
+
+void int_handler(int sig) {
+        sigintRaised = 1;
+}
 
 int main(void)
 {
@@ -28,14 +34,25 @@ int main(void)
                 .sin_port = htons(PORT),
                 .sin_addr.s_addr = INADDR_ANY
         };
+        const struct sigaction act = {
+                .sa_handler = int_handler,
+                .sa_flags = SA_RESTART
+        };
 
         sockfd = socket(AF_INET, SOCK_STREAM, 0);
         if (sockfd == -1) {
-                handle_error("socket");
+                perror("socket");
+                goto clean_fd;
+        }
+
+        if(sigaction(SIGINT, &act, NULL) == -1) {
+                perror("sigaction");
+                goto clean_fd;
         }
 
         if (connect(sockfd, (const struct sockaddr *) &addr, sizeof(addr)) == -1) {
-                handle_error("connect");
+                perror("connect");
+                goto clean_fd;
         }
 
         epollfd = async_init();
@@ -49,19 +66,22 @@ int main(void)
                 wrefresh(input_window);
                 nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
                 if (nfds == -1) {
-                        handle_error("epoll_wait");
+                        perror("epoll_wait");
+                        goto clean;
                 }
 
                 for (int i = 0; i < nfds; i++) {
                         if (events[i].data.fd == STDIN_FILENO) { //from stdin
                                 char buffer[1] = {0};
                                 if (read(events[i].data.fd, buffer, 1) == -1) {
-                                        handle_error("read");
+                                        perror("read");
+                                        goto clean;
                                 }
 
                                 if (buffer[0] == '\r') { //end of the message, send it
                                         if (send(sockfd, buffer_message, sizeof(buffer_message), 0) == -1) {
-                                                handle_error("send");
+                                                perror("send");
+                                                goto clean;
                                         }
                                         memset(buffer_message, 0, sizeof(buffer_message));
                                         i_message = 0;
@@ -87,10 +107,12 @@ int main(void)
                                 struct tm *tm_s;
                                 long int status = recv(events[i].data.fd, buffer, sizeof(buffer), 0);
                                 if (status == -1) {
-                                        handle_error("recv");
+                                        perror("recv");
+                                        goto clean;
                                 }
                                 else if (status == 0) { //connection to server closed
-                                        handle_error("recv");
+                                        perror("recv");
+                                        goto clean;
                                 }
 
                                 timep = time(NULL);
@@ -100,15 +122,17 @@ int main(void)
                                                                 tm_s->tm_min, tm_s->tm_sec, MAXMSG, buffer);
                                 j++;
                         } else {
-                                handle_error("fd");
+                                perror("fd");
+                                goto clean;
                         }
                 }  
         }
 
-        if (close(sockfd) == -1) {
-                handle_error("close");
-        }
-
+        clean:
         endwin();
+
+        clean_fd:
+        close(sockfd);
+
         return 0;
 }
