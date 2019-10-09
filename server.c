f48730ee7f3e297d620a4136c89ff70893dc0268
Corentin Musard <corentinmusard@gmail.com>
2019-10-09 22:44:00 +0200

    server: add a clean exit when server receive SIGINT

--- server.c
+++ server.c
@@ -9,49 +9,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <signal.h>
 
 #include "utils.h"
 #include "log.h"
 #include "asynchronous.h"
 
+volatile sig_atomic_t sigintRaised = 0;
+
+void int_handler(int sig) {
+        sigintRaised = 1;
+}
+
 int main(void)
 {
         struct epoll_event events[MAX_EVENTS];
         int conn_sock, nfds, epollfd;
-        int sockfd;
+        int server_sock_fd;
         const struct sockaddr_in addr = {
                 .sin_family = AF_INET,
                 .sin_port = htons(PORT),
                 .sin_addr.s_addr = INADDR_ANY
         };
+        const struct sigaction act = {
+                .sa_handler = int_handler,
+                .sa_flags = SA_RESTART
+        };
 
-        sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-        if (sockfd == -1) {
-                handle_error("socket");
+        server_sock_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+        if (server_sock_fd == -1) {
+                perror("socket");
+                goto clean_server_fd;
         }
 
-        if (bind(sockfd, (const struct sockaddr *) &addr, sizeof(addr)) == -1) {
-                handle_error("bind");
+        if(sigaction(SIGINT, &act, NULL) == -1) {
+                perror("sigaction");
+                goto clean_server_fd;
         }
 
-        if (listen(sockfd, LISTEN_BACKLOG) == -1) {
-                handle_error("listen");
+        if (bind(server_sock_fd, (const struct sockaddr *) &addr, sizeof(addr)) == -1) {
+                perror("bind");
+                goto clean_server_fd;
+        }
+
+        if (listen(server_sock_fd, LISTEN_BACKLOG) == -1) {
+                perror("listen");
+                goto clean_server_fd;
         }
 
         epollfd = async_init();
-        register_event(epollfd, sockfd, EPOLLIN);
+        register_event(epollfd, server_sock_fd, EPOLLIN);
 
-        while (1) {
+        while (!sigintRaised) {
                 nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
-                if (nfds == -1) {
-                        handle_error("epoll_wait");
+                if (nfds == -1 && errno != EINTR) {
+                        perror("epoll_wait");
+                        goto clean;
                 }
 
                 for (int i = 0; i < nfds; i++) {
-                        if (events[i].data.fd == sockfd) { //event from server socket
-                                conn_sock = accept4(sockfd, NULL, NULL, SOCK_NONBLOCK);
+                        if (events[i].data.fd == server_sock_fd) { //event from server socket
+                                conn_sock = accept4(server_sock_fd, NULL, NULL, SOCK_NONBLOCK);
                                 if (conn_sock == -1) {
-                                        handle_error("accept");
+                                        perror("accept");
+                                        goto clean;
                                 }
                                 register_event(epollfd, conn_sock, EPOLLIN | EPOLLET);
                                 info("Connection open: %d\n", conn_sock);
@@ -71,7 +92,8 @@ int main(void)
                                 long int status = recv(events[i].data.fd, buffer, sizeof(buffer), 0);
 
                                 if (status == -1) { //error
-                                        handle_error("recv");
+                                        perror("recv");
+                                        goto clean;
                                 } else if (status == 0) { //connection closed
                                         info("Connection closed: %d\n", events[i].data.fd);
                                         close(events[i].data.fd);
@@ -84,9 +106,13 @@ int main(void)
                 }
         }
 
-        if (close(sockfd) == -1) {
-                handle_error("close");
+        clean:
+        for (int i = 0; i < MAX_EVENTS; ++i) { //close each sockets still open
+                close(events[i].data.fd);
         }
+
+        clean_server_fd:
+        close(server_sock_fd);
 
         return 0;
 }
