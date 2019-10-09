28399d0663d93569b4ee96775aba7d60658aad8c
Corentin Musard <corentinmusard@gmail.com>
2019-10-09 22:56:05 +0200

    remove unused macro handle_error

--- utils.h
+++ utils.h
@@ -1,12 +1,6 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
-#include <curses.h>
-#include <stdlib.h>
-
-#define handle_error(msg) \
-           do { perror(msg); endwin(); exit(EXIT_FAILURE); } while (0)
-
 // client and server
 #define PORT 5000
 #define MAXMSG 100
