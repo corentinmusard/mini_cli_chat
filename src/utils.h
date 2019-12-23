4902fb9e94a45b47bb06c0fa4527d9ba47ed3aa5
Corentin Musard <corentinmusard@gmail.com>
2019-12-23 14:41:26 +0100

    add documentation

--- src/utils.h
+++ src/utils.h
@@ -1,15 +1,39 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-// client and server
+/*********************************** client and server ***********************************/
+
+/**
+ * server port
+ */
 #define PORT 5000
+
+/**
+ * Maximum length of a message
+ */
 #define MAXMSG 100
+
+/**
+ * Maximum events to be returned by epoll_wait
+ * See man epoll_wait(2) for more infos
+ */
 #define MAX_EVENTS 10
 
-// client only
+/************************************** client only **************************************/
 
-// server only
+//None
+
+/************************************** server only **************************************/
+
+/**
+ * Maximum length to which the queue of pending connections for server_sock_fd may grow. 
+ * See man listen(2) for more infos
+ */
 #define LISTEN_BACKLOG 50
+
+/**
+ * Message sent by server to a client after sucessful connection
+ */
 #define CHAT_BANNER "Welcome to the basic chat!"
 
 #endif
