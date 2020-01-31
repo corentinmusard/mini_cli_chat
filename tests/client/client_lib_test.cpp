4aeffafd63f50a41a50860b6aac2c0a74d42a9f3
Corentin Musard <corentinmusard@gmail.com>
2020-01-31 21:31:45 +0100

    test: client: add some tests

---
+++ tests/client/client_lib_test.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+#include <curses.h>
+
+#include "client/client_lib.h"
+
+class InputTest : public ::testing::Test
+{
+protected:
+    void SetUp() override {
+        input = input_init();
+    }
+
+    void TearDown() override {
+        free_input(input);
+    }
+
+    Input *input;
+};
+
+TEST_F(InputTest, input_init)
+{
+    ASSERT_EQ(input->i, 0);
+}
+
+
+class MessagesTest : public ::testing::Test
+{
+protected:
+    void SetUp() override {
+        messages = messages_init();
+    }
+
+    void TearDown() override {
+        free_messages(messages);
+    }
+
+    Messages *messages;
+};
+
+TEST_F(MessagesTest, messages_init)
+{
+    ASSERT_EQ(messages->y, 0);
+}
+
